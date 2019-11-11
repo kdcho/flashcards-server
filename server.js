bf1da6c0698eb5b14403f1f9bdb518c5c4c8c329
Krystian Chodak <kdchodak@gmail.com>
2019-11-11 16:03:21 +0100

    Add patch & post

--- server.js
+++ server.js
@@ -2,12 +2,36 @@ const express = require('express')
 const uid = require('uid')
 
 const app = express()
+app.use(express.json())
 let cards = require('./cards.json').map(card => ({ ...card, id: uid() }))
 
 app.listen(3334, () => console.log('Express ready'))
 
 app.get('/cards', (req, res) => {
   res.json(cards)
+})
+
+app.get('/cards/:id', (req, res) => {
+  const id = req.params.id
+  /*   index = cards.findIndex(card => card.id === id)
+  res.json(cards[index]) */
+  card = cards.find(card => card.id === id)
+  res.json(card)
+})
+
+app.post('/cards', (req, res) => {
+  const newCard = { ...req.body, id: uid() }
+  //cards.unshift(newCard)
+  cards = [newCard, ...cards]
+  res.json(newCard)
+})
+
+app.patch('/cards/:id', (req, res) => {
+  const patch = req.body
+  index = cards.findIndex(card => card.id === req.params.id)
+  card = { ...cards[index], ...patch }
+  cards[index] = card
+  res.json(card)
 })
 
 app.delete('/cards/:id', (req, res) => {
